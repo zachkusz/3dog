e2f2ff276d6d8dd96bfcfeb9ce4a41e90fba0fa3
Zach Kusz <zskusz@gmail.com>
2016-05-25 15:37:02 -0500

    css fix

--- server/public/scripts/controllers/jakeController.js
+++ server/public/scripts/controllers/jakeController.js
@@ -1,4 +1,9 @@
 myApp.controller('JakeController', ['$scope', function($scope) {
-  console.log('Jake controller running');
-  $scope.message = "Jake Controller!";
+  console.log('Courage controller running');
+  $scope.name = "Jake the Dog";
+  $scope.show = 'Adventure Time!';
+  $scope.run =  'April 5, 2010 - Present';
+  $scope.episodes = '233 Episodes';
+  $scope.creator = 'Pendelton Ward';
+  $scope.voiceActor = 'John DiMaggio';
 }]);
--- server/public/scripts/controllers/scoobyController.js
+++ server/public/scripts/controllers/scoobyController.js
@@ -1,4 +1,9 @@
 myApp.controller('ScoobyController', ['$scope', function($scope) {
-  console.log('scooby controller running');
-  $scope.message = "Scooby Controller!";
+  console.log('scoob controller running');
+  $scope.name = "Scooby-Doo";
+  $scope.show = 'Sooby-Doo, Where Are You!';
+  $scope.run =  'Sept. 13, 1969 – Nov. 4, 1978';
+  $scope.episodes = '34 Episodes';
+  $scope.creator = 'Joe Ruby';
+  $scope.voiceActor = 'Don Messick';
 }]);
